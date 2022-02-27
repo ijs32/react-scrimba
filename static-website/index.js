68823e205910b5a3200b0aa44902c59a878cbcb5
Ian S <iansilber32@gmail.com>
2022-02-27 18:09:03 -0500

    static page with react

---
+++ static-website/index.js
@@ -0,0 +1,15 @@
+const header = (
+  <div>
+    <img src="react-logo.png" style={{ width: "40px" }} />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100k stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(header, document.getElementById("root"));
