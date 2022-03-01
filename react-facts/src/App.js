27fdcaf5a6c707f2b57b70bee16363ad449e396c
Ian S <iansilber32@gmail.com>
2022-02-28 21:51:18 -0500

    initial layout for the react-facts project

---
+++ react-facts/src/App.js
@@ -0,0 +1,13 @@
+import MainContent from "../components/MainContent";
+import Header from "../components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
